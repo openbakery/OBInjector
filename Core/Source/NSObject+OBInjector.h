6adc3e82efa1feb2b503c98db97f3ee4b46b6dd7
René Pirringer <rene@pirringer.at>
2015-09-07 09:13:55 +0200

    added appledoc documentation

--- Core/Source/NSObject+OBInjector.h
+++ Core/Source/NSObject+OBInjector.h
@@ -9,12 +9,16 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-
+/**
+ * Category so that every object can inject dependencies to other objects.
+ */
 @interface NSObject (OBInjector)
 
 /**
  * Injects the instances to the registered properties of the given object.
  * Note that only values are injected where the property is nil.
+ *
+ * @param injectTo Object where the dependencies should be injected
  */
 - (void)injectDependenciesTo:(NSObject *)injectTo;
 
--- Core/Source/OBInjector.h
+++ Core/Source/OBInjector.h
@@ -10,7 +10,9 @@
 
 typedef id<NSObject> (^OBInjectorCreateInstanceBlock)(void);
 
-
+/**
+ * The OBInjectorDelegate protocol can be implemented when a instance need to know when the dependency injection took place
+ */
 @protocol OBInjectorDelegate
 
 @optional
@@ -24,12 +26,17 @@ typedef id<NSObject> (^OBInjectorCreateInstanceBlock)(void);
 
 @end
 
+/**
+ * The OBInjector class handles the injects dependencies to properties instances that where registerd.
+ * 
+ */
 @interface OBInjector : NSObject
 
 
 /**
  * Injects the instances to the registered properties of the given object.
  * Note that only values are injected where the property is nil.
+ * @param injectTo Object where the dependencies should be injected
  */
 - (void)injectDependenciesTo:(NSObject *)injectTo;
 
@@ -37,18 +44,24 @@ typedef id<NSObject> (^OBInjectorCreateInstanceBlock)(void);
 /**
  * Registers a property that can be injected to a object.
  * The given instance is then set as property value of the object using the injectDependenciesTo method.
+ *
+ * @param property Name of the property
+ * @param instance Object that should be injected
  */
 - (void)registerProperty:(NSString *)property withInstance:(id<NSObject>)instance;
 
 /**
  * Registers a property that can be injected to a object.
  * When the property is injected then the given block is called and the result is injected.
+ *
+ * @param property Name of the property
+ * @param block Block that is executed and the returned result is injected
  */
 - (void)registerProperty:(NSString *)property withBlock:(OBInjectorCreateInstanceBlock)block;
 
 /**
  * Method to get the instance of the given class
- *
+ * @param clazz Class where a instance should be returned
  * @returns an instance for the giving class
  */
 - (id)instanceForClass:(Class)clazz;
