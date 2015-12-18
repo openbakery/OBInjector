72f80837fe83973521fbc3910ec598539d527726
René Pirringer <rene@pirringer.at>
2015-12-18 11:13:20 +0100

    updated the documentation

--- Core/Source/OBInjectorController.h
+++ Core/Source/OBInjectorController.h
@@ -1,18 +1,30 @@
 //
-// Created by Rene Pirringer on 18.12.15.
-// Copyright (c) 2015 Rene Pirringer. All rights reserved.
+//
+// Author: René Pirringer
+//
 //
 
 #import <Foundation/Foundation.h>
 
 @class OBInjector;
 
-
+/**
+ * The OBInjectorController is the central class to create and get access to the injector
+ */
 @interface OBInjectorController : NSObject
 
+/**
+ * Method to get the instance of the OBInjectorController
+ *
+ * @returns an OBInjectorController instance
+ */
 + (OBInjectorController *)sharedController;
 
-
+/**
+ * Property to the injector instance
+ *
+ * @returns an injector instance
+ */
 @property(nonatomic, readonly) OBInjector *injector;
 
 @end
--- Core/Source/OBInjectorController.m
+++ Core/Source/OBInjectorController.m
@@ -1,6 +1,7 @@
 //
-// Created by Rene Pirringer on 18.12.15.
-// Copyright (c) 2015 Rene Pirringer. All rights reserved.
+//
+// Author: René Pirringer
+//
 //
 
 #import "OBInjectorController.h"
