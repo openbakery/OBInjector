5363b2838304779e354cbafcfe3bc9fd2d3c3c02
René Pirringer <rene@pirringer.at>
2016-03-23 15:25:48 +0100

    defined framework

--- Core/Source/OBInjector.h
+++ Core/Source/OBInjector.h
@@ -1,3 +1,10 @@
 
-#import "OBInjectorController.h"
-#import "OBPropertyInjector.h"
+
+#ifndef _OBINJECTOR_
+#define _OBINJECTOR_
+
+#import <OBInjector/OBInjectorController.h>
+#import <OBInjector/OBPropertyInjector.h>
+
+
+#endif /* _OBINJECTOR_ */
